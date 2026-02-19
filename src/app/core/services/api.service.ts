eef1f8fdecdfe61b08bf59509af17285808b83b1
Yasmin <yasmin.lino@sisand.com.br>
2026-02-19 18:55:29 -0300

    Ajustes package

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
-import { environment } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
