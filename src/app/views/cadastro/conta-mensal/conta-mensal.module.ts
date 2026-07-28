f8d9001bccdc6388c91d22d7efc5c3e716abb7f5
Yasmin <yasmin.lino@sisand.com.br>
2026-07-27 21:39:20 -0300

    Projeto funcionando antes da migração para Angular 17

--- src/app/views/cadastro/conta-mensal/conta-mensal.module.ts
+++ src/app/views/cadastro/conta-mensal/conta-mensal.module.ts
@@ -14,7 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     ContaMensalListComponent,
     ContaMensalFormComponent,
   ],
-  imports: [
+  imports: [ 
     SharedModule,
     ReactiveFormsModule,
     ContaMensalRoutingModule,
